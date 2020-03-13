9344153e20418dfa526773e73051e26e2ff78cbd
R6WEN <style1223style89@gmail.com>
2020-03-13 20:53:42 +0800

    加入專案檔案。

---
+++ test0313/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace test0313
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            int hp = 100;
+            while (true)
+            {
+                if (hp < 0)
+                {
+                    Console.WriteLine("蝙蝠湯");
+                    break;
+
+                }
+                string input = Console.ReadLine();
+                int attack = int.Parse(input);
+                hp = hp - attack;
+                Console.WriteLine("hp=" + hp);
+              
+
+
+
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
